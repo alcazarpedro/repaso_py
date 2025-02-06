d610fda145fd04acb33b72f2e20839c5fa3680f5
Pedro Luis Alcazar <alcazarjimenezpedro@gmail.com>
2025-02-05 20:13:54 -0600

    funcion suma dos numeros enteros y da como resultado un numero entero

---
+++ suma_dos_numeros.py
@@ -0,0 +1,8 @@
+
+def suma_numeros(a:int,b:int)-> int:
+    return a+b
+
+a = int(input('digita un numero: '))
+b = int(input('digita un numero: '))
+out = suma_numeros(a,b)
+print(f'resultado : {out}')
