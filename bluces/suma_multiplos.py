3d86237508c35aafc8f403188c4af701e04a671c
Pedro Luis Alcazar <alcazarjimenezpedro@gmail.com>
2025-05-03 16:42:00 -0600

    update suma_multiplos.py

--- bluces/suma_multiplos.py
+++ bluces/suma_multiplos.py
@@ -3,7 +3,7 @@ def multiplos_primos(valores:list) -> int:
     multiplo_tres_cinco = []
     
     for elemento in valores:
-        if elemento % 3 == 0 or elemento % 5 ==0 :
+        if elemento % 3 == 0 or elemento % 5 == 0 :
             multiplo_tres_cinco.append(elemento)
     suma = reduce(lambda a,b : a+b,multiplo_tres_cinco)    
     return f'la suma de los multiplos 3 o 5 es : {suma}'
